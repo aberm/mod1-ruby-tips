e863cc9f41706f068bf2c8c4a8ea8794e591e24c
aberm <abermendel@gmail.com>
2018-12-20 10:37:52 -0500

    Done

--- solution.rb
+++ solution.rb
@@ -1,1 +1,17 @@
+def palindrome?(str)
 
+  i = 0
+  while i < str.length
+    if str[i].downcase != str[-(i+1)].downcase
+      puts "no"
+      break
+    else
+      i += 1
+    end
+  end
+
+  str.downcase!
+  return true if str == str.reverse && str.length > 0 else false
+end
+
+puts palindrome?("non")
